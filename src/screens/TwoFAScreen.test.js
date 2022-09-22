abca1e8832489e0593e3a3f094f862cb82743644
Reshad Aziz <raziz@guidedchoice.com>
2022-09-22 14:49:30 -0700

    added tests for 2fa

---
+++ src/screens/TwoFAScreen.test.js
@@ -0,0 +1,51 @@
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import { mock } from 'jest-mock-extended';
+import { AuthStore } from '../AuthStore';
+
+import TwoFAScreen from "./TwoFAScreen";
+import { AuthProvider } from '../AuthContext';
+
+describe('TwoFAScreen', () => {
+    it('not calling attemptLoginTwoFactor api', async () => {
+
+        const authStore = new AuthStore();
+
+        authStore.attemptLoginTwoFactor = jest.fn();
+
+        render(
+            <AuthProvider authStore={authStore}>
+                <TwoFAScreen />
+            </AuthProvider>
+        );
+
+        const button = screen.getByRole('button');
+        await act(() => button.click())
+
+        expect(authStore.attemptLoginTwoFactor).not.toHaveBeenCalled();
+    });
+
+    it('calling attemptLoginTwoFactor api', async () => {
+
+        const authStore = new AuthStore();
+
+        const attemptLoginTwoFactor = jest.fn();
+        authStore.attemptLoginTwoFactor = attemptLoginTwoFactor;
+
+        render(
+            <AuthProvider authStore={authStore}>
+                <TwoFAScreen />
+            </AuthProvider>
+        );
+
+        const twoFA = '123456'
+
+        const twoFAInput = screen.getByTestId('code').querySelector('input');
+
+        await fireEvent.change(twoFAInput, { target: { value: twoFA } });
+
+        const button = screen.getByRole('button');
+        await act(() => { button.click()})
+
+        expect(attemptLoginTwoFactor).toHaveBeenCalledWith(twoFA);
+    });
+});
