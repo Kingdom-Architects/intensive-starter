1011ca12d0b8280c4007af20ccb0d3552fa42da0
Reshad Aziz <raziz@guidedchoice.com>
2022-09-23 10:13:26 -0700

    added investments screen

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,12 +6,14 @@ import {
   Link
 } from "react-router-dom";
 import LoginScreen from "../screens/LoginScreen";
+import Investments from "../screens/Investments";
 import { TwoFAScreen } from "../screens/TwoFAScreen";
 
 export default function AppRoutes() {
     return ( 
         <Router>
             <Routes>
+                <Route path="/investments" element={<Investments />} />
                 <Route path="/twofa" element={<TwoFAScreen />} />
                 <Route path="/" element={<LoginScreen />} />
             </Routes>
