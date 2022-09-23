33fad38cfdc864988412b2d2cb5d92493a733be5
Michael Martinelli <mmartinelli@guidedchoice.com>
2022-09-23 10:15:07 -0700

    update testcafe impersonate test

--- src/testcafe/index.ts
+++ src/testcafe/index.ts
@@ -1,10 +1,28 @@
 import { ClientFunction, Selector } from "testcafe";
+import createAccount from "../system-state/commands/createAccount";
+import runScenario from "../system-state/runScenario";
 
-const impersonate = ClientFunction(() =>
+const storedValues: Record<string, any> = {};
+const stubbedWindow = {
+  localStorage: {
+    getItem: (key: string) => {
+      return storedValues[key];
+    },
+    setItem: (key: string, value: any) => {
+      storedValues[key] = value;
+    },
+    removeItem: (key: string) => {
+      delete storedValues[key];
+    },
+  },
+};
+
+// @ts-ignore
+globalThis.window = stubbedWindow;
+
+const impersonate = ClientFunction((token: string) =>
   // @ts-ignore
-  window.impersonate(
-    "eyJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJNT0JJTEVfVVNFUiIsImFjY291bnRJZCI6MjQyODQ5NCwic2NvcGUiOiIiLCJkYlVzZXIiOjAsInN1YiI6IjAiLCJleHAiOjE2NjQ3MjkzMjcsImVudiI6InVhdCIsImlhdCI6MTY2Mzg2NTMyN30.bOef_vBxVWSiCTd9vezDDEvkp6ecjlgvyINjSqrhLfPj0QVGaKTse05KwDCN0F1BAxZ0b-EtbR1c-7nYfJsvWg"
-  )
+  window.impersonate(token)
 );
 
 const getToken = ClientFunction(() =>
@@ -18,7 +36,55 @@ const getToken = ClientFunction(() =>
 fixture`Getting Started`.page`http://127.0.0.1:3000`;
 
 test("Impersonate", async () => {
-  await impersonate();
-  const token = await getToken();
+  const storage = await runScenario(async (state) => {
+    await state.execute(createAccount, 1, {
+      firstName: "Test",
+      lastName: "User",
+      emailAddress: '=RANDOM_EMAIL("test.guidedchoice.com")',
+      password: "P@ssw0rd!!",
+      birthdate: "1997-04-04T00:00:00Z",
+      product: "advice",
+      gender: "M",
+      maritalStatus: true,
+      includeSpouse: true,
+      preferredName: "McTester",
+      finances: {
+        annualIncome: 100000,
+        taxState: "TX",
+        investmentPreference: "LOW_COST",
+        taxFilingStatus: 2,
+        creditScore: 800,
+        eligibleForHsa: false,
+        selfEmployed: false,
+      },
+      contactInfo: {
+        address1: "1234 Test Lane",
+        address2: undefined,
+        city: "Austin",
+        state: "TX",
+        zipCode: "77801",
+        mobilePhone: "555-234-2344",
+        phoneNumber: "555-234-2344",
+      },
+      spouse: {
+        firstName: "Chloe",
+        lastName: "LastName",
+        birthdate: "1999-04-12",
+        preferredName: "McSpouse",
+        creditScore: 800,
+        annualIncome: 100000,
+        otherIncome: 0,
+        gender: "F",
+      },
+      onboarding: {
+        personalDetails: true,
+        accounts: true,
+        budgetDetails: true,
+        adviceAcknowledged: false,
+        termsAndConditions: false,
+      },
+    });
+  });
+  const { token } = storage.get("accessToken", 1);
   console.log(token);
 });
